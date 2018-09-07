901e6990a4f5c5496b5372103b0e42834f7598fe
Yuol96 <huangyc96@gmail.com>
2018-09-07 16:45:14 +0800

    add in TripletLoss module

--- lib/model/one_shot/one_shot_resnet.py
+++ lib/model/one_shot/one_shot_resnet.py
@@ -6,11 +6,54 @@ sys.path.append('/unsullied/sharefs/huangyucheng/data/public_git/pytorch-one-sho
 from model.faster_rcnn.resnet import resnet101
 from model.one_shot.triplet_loss import batch_hard_triplet_loss
 
+class TripletLoss(nn.Module):
+  """Triplet loss with hard positive/negative mining.
+  Reference:
+  Hermans et al. In Defense of the Triplet Loss for Person Re-Identification. arXiv:1703.07737.
+  Code imported from https://github.com/Cysu/open-reid/blob/master/reid/loss/triplet.py.
+  Args:
+      margin (float): margin for triplet.
+  """
+  def __init__(self, margin=0.3, mutual_flag = False):
+    super(TripletLoss, self).__init__()
+    self.margin = margin
+    self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+    self.mutual = mutual_flag
+
+  def forward(self, inputs, targets):
+    """
+    Args:
+        inputs: feature matrix with shape (batch_size, feat_dim)
+        targets: ground truth labels with shape (num_classes)
+    """
+    n = inputs.size(0)
+    # inputs = 1. * inputs / (torch.norm(inputs, 2, dim=-1, keepdim=True).expand_as(inputs) + 1e-12)
+    # Compute pairwise distance, replace by the official when merged
+    dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)
+    dist = dist + dist.t()
+    dist.addmm_(1, -2, inputs, inputs.t())
+    dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+    # For each anchor, find the hardest positive and negative
+    mask = targets.expand(n, n).eq(targets.expand(n, n).t())
+    dist_ap, dist_an = [], []
+    for i in range(n):
+      dist_ap.append(dist[i][mask[i]].max().unsqueeze(0))
+      dist_an.append(dist[i][mask[i] == 0].min().unsqueeze(0))
+    dist_ap = torch.cat(dist_ap)
+    dist_an = torch.cat(dist_an)
+    # Compute ranking hinge loss
+    y = torch.ones_like(dist_an)
+    loss = self.ranking_loss(dist_an, dist_ap, y)
+    if self.mutual:
+      return loss, dist
+    return loss
+
 class one_shot_resnet(nn.Module):
-  def __init__(self, embedding_dim, model_path, pretrained=False):
+  def __init__(self, embedding_dim, model_path, margin, pretrained=False):
     self.model_path = model_path
     self.pretrained = pretrained
     self.embedding_dim = embedding_dim
+    self.margin = margin
 
     super(one_shot_resnet, self).__init__()
 
@@ -65,6 +108,8 @@ class one_shot_resnet(nn.Module):
     self.RCNN_base.apply(set_bn_fix)
     self.RCNN_top.apply(set_bn_fix)
 
+    self.tripletloss = TripletLoss(margin=self.margin)
+
   def _head_to_tail(self, pool5):
     fc7 = self.RCNN_top(pool5).mean(3).mean(2)
     return fc7
@@ -84,7 +129,7 @@ class one_shot_resnet(nn.Module):
       self.RCNN_base.apply(set_bn_eval)
       self.RCNN_top.apply(set_bn_eval)
 
-  def forward(self, im_data, labels, margin):
+  def forward(self, im_data, labels):
     """
     Input:
       - im_data: (batch_size, num_channels, img_height, img_width)
@@ -101,7 +146,8 @@ class one_shot_resnet(nn.Module):
 
     embeddings = self.Linear_top(pooled_feat)
 
-    loss = batch_hard_triplet_loss(labels, embeddings, margin, squared=False)
+    # loss = batch_hard_triplet_loss(labels, embeddings, margin, squared=False)
+    loss = self.tripletloss(embeddings, labels)
 
     return embeddings, loss
 
