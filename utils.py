b7cd8e41634b42082993674e826e287a74b9ee37
Yuol96 <huangyc96@gmail.com>
2018-09-28 11:31:12 +0800

    add iou compute function

--- utils.py
+++ utils.py
@@ -17,3 +17,71 @@ def compute_mat_dist(a,b,squared=False):
         dist = dist.sqrt()
         dist *= (1-mask.float())
     return dist
+
+def bb_intersection_over_union(boxA, boxB):
+    """
+    copied from https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
+    """
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+ 
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+ 
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+ 
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+ 
+    # return the intersection over union value
+    return iou
+
+def computeIOU_torch(boxA, boxB):
+    """
+    Input:
+        - boxA: (m, 4) torch.Tensor
+        - boxB: (n, 4) torch.Tensor
+    Intermediate Variables:
+        - xA, yA, xB, yB, interArea: (m, n)
+        - boxAArea: (m)
+        - boxBArea: (n)
+    Output:
+        - iou: (m, n)
+    """
+    xA = torch.max(boxA[:,0].unsqueeze(1), boxB[:,0].unsqueeze(1).t())
+    yA = torch.max(boxA[:,1].unsqueeze(1), boxB[:,1].unsqueeze(1).t())
+    xB = torch.min(boxA[:,2].unsqueeze(1), boxB[:,2].unsqueeze(1).t())
+    yB = torch.min(boxA[:,3].unsqueeze(1), boxB[:,3].unsqueeze(1).t())
+
+    interArea = (xB-xA+1).clamp(min=0) * (yB-yA+1).clamp(min=0)
+    
+    boxAArea = (boxA[:,2]-boxA[:,0]+1) * (boxA[:,3]-boxA[:,1]+1)
+    boxBArea = (boxB[:,2]-boxB[:,0]+1) * (boxB[:,3]-boxB[:,1]+1)
+
+    iou = interArea / (boxAArea.unsqueeze(1) + boxBArea.unsqueeze(1).t() - interArea).float()
+    return iou
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
